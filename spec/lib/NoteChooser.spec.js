39c58bbe50e8889661d9ac06555d45f40c7029c2
Lewis <davanzo.lewis@gmail.com>
2021-01-22 11:28:51 -0500

    change more octaves

--- spec/lib/NoteChooser.spec.js
+++ spec/lib/NoteChooser.spec.js
@@ -29,12 +29,12 @@ it('chooses last note', () => {
   setRandom(0)
   let nc = prep('C7', 'C7', null, 'C', 'down')
   expect(nc.firstNote()).toEqual('C3')
-  expect(nc.lastNote()).toEqual('B0')
+  expect(nc.lastNote()).toEqual('B1')
 
   setRandom(0.65)
   nc = prep('C7', 'C7', null, 'C')
   expect(nc.firstNote()).toEqual('C3')
-  expect(nc.lastNote()).toEqual('A#1')
+  expect(nc.lastNote()).toEqual('A#2')
 })
 
 it('next bar first note', () => {
@@ -45,10 +45,10 @@ it('next bar first note', () => {
 
   nc = prep('C7', 'C7', null, 'C', 'down')
   expect(nc.firstNote()).toEqual('C3')
-  expect(nc.runNextBarFirstNote()).toEqual('B1')
+  expect(nc.runNextBarFirstNote()).toEqual('B2')
 
   setRandom(0.5)
   nc = prep('Dm7', 'Dm7', null, 'C', 'down')
   expect(nc.firstNote()).toEqual('D3')
-  expect(nc.runNextBarFirstNote()).toEqual('D1')
+  expect(nc.runNextBarFirstNote()).toEqual('D2')
 })
